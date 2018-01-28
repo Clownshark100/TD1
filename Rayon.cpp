d5e5baa6c0042e410f43f42788e33c2903b3eabe
Clownshark100 <bdamnixel@live.com>
2018-01-27 20:39:54 -0500

    implementation de Rayon (#4)
    implementation de Rayon

---
+++ Rayon.cpp
@@ -0,0 +1,68 @@
+#include "Rayon.h"
+
+Rayon::Rayon() : categorie_("inconnu"), tousProduits_(nullptr), capaciteProduits_(0), nombreProduits_(0) {}
+
+Rayon :: Rayon(string cat) : categorie_(cat), tousProduits_(nullptr), capaciteProduits_(0), nombreProduits_(0){}
+
+string Rayon ::  obtenirCategorie()
+{
+	return categorie_;
+}
+
+Produit** Rayon :: obtenirTousProduits()
+{
+	return  tousProduits_;
+}
+
+int Rayon :: obtenirCapaciteProduits()
+{
+	return capaciteProduits_;
+}
+
+int Rayon :: obtenirNombreProduits()
+{
+	return nombreProduits_;
+}
+
+void Rayon::modifierCategorie(string cat)
+{
+	categorie_ = cat;
+}
+
+void Rayon :: ajouterProduit(Produit * produit)
+{
+	if (nombreProduits_ < capaciteProduits_){
+		tousProduits_[nombreProduits_++] = produit;
+	}
+	else
+	{
+		Produit** nouvelleListe = new Produit*[capaciteProduits_ + 5]; // crée une nouvelle liste avec une capacité augmentée de 5
+
+		for (int i = 0; i < capaciteProduits_; i++) 
+			nouvelleListe[i] = tousProduits_[i]; // transfère les produits de la liste initiale à la nouvelle liste
+		
+		delete[] tousProduits_; // désalloue la liste initiale
+
+		tousProduits_ = nouvelleListe; // la nouvelle liste devient la liste initiale
+
+		capaciteProduits_ += 5; // incrémente l'attribut capaciteProduits par 5
+
+		tousProduits_[nombreProduits_++] = produit; // ajoute le produit dans la nouvelle liste
+	}	
+}
+
+void Rayon::afficher() 
+{
+	cout << "Categorie : " << categorie_ << endl << "Nombre de produits : " 
+		<< nombreProduits_ << endl << " Capacite : " << capaciteProduits_ << endl;
+	
+	for (int i = 0; i < nombreProduits_; i++)
+	{
+		tousProduits_[i]->afficher();
+	}
+}
+
+
+
+
+
