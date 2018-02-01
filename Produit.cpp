d52aa0546ed11d217e713b3f0198b9b9fccff671
Loïc <lolebc@labos.polymtl.ca>
2018-02-01 15:04:24 -0500

    Commentaires mania

--- Produit.cpp
+++ Produit.cpp
@@ -1,37 +1,64 @@
+/**************************************************
+* Titre: Travail pratique #1 - panier.cpp
+* Date: 20 janvier 2018
+* Auteur: Loic LeBlanc et Daniel Nahum
+**************************************************/
+
 #include "Produit.h"
 #include <stdio.h>
 
+/**
+* Constructeur par defaut de Produit
+*/
 Produit::Produit() : nom_("outil"), reference_(0), prix_(0.0) {
 }
 
+/**
+* Constructeur par parametres de Produit
+*/
 Produit::Produit(string nom, int reference, double prix) 
 	: nom_(nom), reference_(reference), prix_(prix){
 }
 
+/**
+* Accesseur de nom_
+*/
 string Produit::obtenirNom() const {
 	return nom_;
 }
-
+/**
+* Accesseur de reference_
+*/
 int Produit::obtenirReference() const {
 	return reference_;
 }
-
+/**
+* Accesseur de prix_
+*/
 double Produit::obtenirPrix() const{
 	return prix_;
 }
-
+/**
+* Modifie la valeur de nom_ par la valeur passee en parametre
+*/
 void Produit::modifierNom(const string nouveauNom) {
 	nom_ = nouveauNom;
 }
-
+/**
+* Modifie la valeur de reference_ par la valeur passee en parametre
+*/
 void Produit::modifierReference(const int nouvelleReference) {
 	reference_ = nouvelleReference;
 }
-
+/**
+* Modifie la valeur de prix_ par la valeur passee en parametre
+*/
 void Produit::modifierPrix(const double nouveauPrix) {
 	prix_ = nouveauPrix;
 }
-
+/**
+* Cette methode affiche le les caracteristiques du produit de facon ergonomique et jolie.
+*/
 void Produit::afficher() const {
 
 	cout << "Details pour: " << nom_ << endl << 
--- client.cpp
+++ client.cpp
@@ -1,52 +1,94 @@
+/**************************************************
+* Titre: Travail pratique #1 - panier.cpp
+* Date: 20 janvier 2018
+* Auteur: Loic LeBlanc et Daniel Nahum
+**************************************************/
+
+
 #include "client.h"
 
+/**
+* Constructeur par parametres de Client
+*/
 Client::Client(string nom, string prenom, int identifiant, string codePostal, long date) 
 	: nom_(nom), prenom_(prenom), identifiant_(identifiant), codePostal_(codePostal), dateNaissance_(date), monPanier_(nullptr)
-{
-}
+{}
+
+/**
+* Destructeur de Client, desalloue le panier.
+*/
 Client::~Client(){
 	delete monPanier_;
 }
+
+/**
+* Accesseur de nom_
+*/
 string Client::obtenirNom() const {
 	return nom_;
 }
 
+/**
+* Accesseur de prenom_
+*/
 string Client::obtenirPrenom() const {
 	return prenom_;
 }
 
+/**
+* Accesseur de identifiant_
+*/
 int Client::obtenirIdentifiant() const {
 	return identifiant_;
 }
 
+/**
+* Accesseur de codePostal_
+*/
 string Client::obtenirCodePostal() const{
 	return codePostal_;
 }
 
+/**
+* Accesseur de dateNaissance_
+*/
 long Client::obtenirDateNaissance() const {
 	return dateNaissance_;
 }
-
+/**
+* Modifie la valeur de nom_ par la valeur passee en parametre
+*/
 void Client::modifierNom(const string nom) {
 	nom_ = nom;
 }
-
+/**
+* Modifie la valeur de prenom_ par la valeur passee en parametre
+*/
 void Client::modifierPrenom(const string prenom) {
 	prenom_ = prenom;
 }
-
+/**
+* Modifie la valeur de identifiant_ par la valeur passee en parametre
+*/
 void Client::modifierIdentifiant(const int identifiant) {
 	identifiant_ = identifiant;
 }
-
+/**
+* Modifie la valeur de codePostal_ par la valeur passee en parametre
+*/
 void Client::modifierCodePostal(const string codePostal) {
 	codePostal_ = codePostal;
 }
 
+/**
+* Modifie la valeur de dateNaissance_ par la valeur passee en parametre
+*/
 void Client::modifierDateNaissance(const long date) {
 	dateNaissance_ = date;
 }
-
+/**
+* Cette methode ajoute un produit au Panier de ce client. Il alloue un Panier s'il y a lieu.
+*/
 void Client::acheter(Produit* produit) {
 	//Si le panier existe
 	if (monPanier_) {
@@ -57,12 +99,17 @@ void Client::acheter(Produit* produit) {
 		monPanier_->ajouter(produit);
 	}
 }
-
+/**
+* Cette methode affiche les produits dans le panier du client de facon ergonomique et jolie.
+*/
 void Client::afficherPanier() const {
 	
 	monPanier_->afficher();
 }
 
+/**
+* Cette methode simule la livraison du panier du client en le vidant.
+*/
 void Client::livrerPanier() {
 	monPanier_->livrer();
 }
